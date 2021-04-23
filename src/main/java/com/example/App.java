f67b864a4d0d03c9afb90eeac20a76eaae600a33
matthias-seibert-tng <61148008+matthias-seibert-tng@users.noreply.github.com>
2021-04-23 08:29:10 +0200

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -24,4 +24,50 @@ public class App {
     public String getStatus() {
         return "OK";
     }
+    
+    private int in = 0;
+    private int out = 0;
+    private BigDecimal pi = new BigDecimal(3.141592653589793238462643);
+
+    // get amount of points inside the circle
+    private int getIn() {
+        return this.in;
+    }
+    // get amount of points outside the circle
+    private int getOut() {
+        return this.out;
+    }
+
+    private BigDecimal getPi() {
+        return this.pi;
+    }
+
+    private double getRand() {
+        Random rd = new Random();
+        return rd.nextDouble();
+    }
+
+    private BigDecimal calcPi() {
+        // multiply the result with 4 because the simulation was only
+        BigDecimal piTmp = BigDecimal.valueOf(4.0 * ((double) this.getIn() / ((double) this.getIn() + (double) this.getOut())));
+        return piTmp;
+    }
+
+    // calculation for n
+    public BigDecimal calculatePiForN(int n) {
+        for (int i = 0; i < n; i++) {
+            // method getRand() provides random values
+            double x = this.getRand();
+            double y = this.getRand();
+            if (x * x + y * y <= 1) {
+            // point is inside the circle
+            this.in++;
+            } else {
+            // point is outside the circle
+            this.out++;
+            }
+        }
+        // print results in the console
+        return this.calcPi();
+    }
 }
